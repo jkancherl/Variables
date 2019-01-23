64d004898eed64ba11aa4dbf35aa5f8233a0f343
GBTC406011UR <GBTC406011UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-01-23 17:09:30 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,59 @@
+package com.company;
+
+import java.sql.Time;
+import java.sql.Timestamp;
+
+public class Main {
+
+    public static void main(String[] args) {
+    System.out.print("This is taking too long");
+        System.out.println("next page");
+
+        //counter top double
+        String counter;
+        counter = "InvoiceApp, InvoceApp2, $orderTotal, _orderTotal, input_string, subtotal, _get_total, Months_per_year";
+        System.out.println(counter);
+                int top;
+        top = 1;
+        System.out.println(top);
+        double top_width = 23.30;
+        System.out.print(top_width);
+
+        //date
+        String date = "01/23/2019";
+        System.out.println(date);
+
+        //Full Name
+        String firstName, lastName;
+        firstName = "John ";
+        lastName = "Doe ";
+        System.out.println(firstName + lastName);
+
+        // new demo
+        String question = "unknown";
+        int x, y, age;
+        x= 32;
+        y= 435;
+        age = 24;
+
+        double temperature;
+        float Temperature; // only take number not characters
+        temperature = 98.6;
+        Temperature = 32.0f;
+
+        System.out.println( "The variable x contains a value of " + x);
+        System.out.print("The value " +y+"is the largest value ");
+        System.out.println("you can store in a integer. ");
+        System.out.println( "I am lying when I say my age is: "+ age);
+        System.out.println("And the question has long since been " + question);
+        System.out.println("If your not sick your temperature is " + temperature);
+        System.out.println("If your are in maryland it is " + Temperature );
+        System.out.println("The variable Temperature is not " + "the same as temperature");
+        System.out.println("The founder of Java is " + firstName + lastName);
+
+
+
+
+
+    }
+}
